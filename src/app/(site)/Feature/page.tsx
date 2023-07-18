0b787b9d56b769273692278d2c77a309ea6a4f06
Anurag Sidhu <anuragsidhu72@gmail.com>
2023-07-18 11:37:12 +0530

    solve

--- src/app/(site)/Feature/page.tsx
+++ src/app/(site)/Feature/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import food from "./food.json"
@@ -16,13 +16,7 @@ interface IAFood{
   Image:string
 }
 const page = () => {
-const [data, setData] = useState<IAFood [] | undefined>()
-const [query, setQuery] = useState('');
-  
-    //Handling the input on our search bar
-    const handleChange = (e:any) => {
-    setQuery(e.target.value)
-    } 
+
 
     // useEffect(() => {
     //   setData(food.AllFoods)
--- src/app/(site)/signUp/page.tsx
+++ src/app/(site)/signUp/page.tsx
@@ -1,32 +1,32 @@
-'use client'
-import React from 'react'
-import { useSession, signIn, signOut } from "next-auth/react"
-import { redirect } from 'next/navigation'
+// 'use client'
+// import React from 'react'
+// import { useSession, signIn, signOut } from "next-auth/react"
+// import { redirect } from 'next/navigation'
 
-const page = () => {
-  // const { data: session } = useSession({
-  //   required:true,
-  //   onUnauthenticated(){
-  //     redirect('/login')
-  //   }
-  // })
+// const page = () => {
+//   // const { data: session } = useSession({
+//   //   required:true,
+//   //   onUnauthenticated(){
+//   //     redirect('/login')
+//   //   }
+//   // })
  
-  const {data: session} = useSession()
-  return (
-    <div>
-      {
-        session?.user ?(
-          <>
-          <p>{session.user.name}</p>
-          </>
-        ):(
-          <button onClick={()=>signIn('google')}>Sign In</button>
-          )
-        }
+//   const {data: session} = useSession()
+//   return (
+//     <div>
+//       {
+//         session?.user ?(
+//           <>
+//           <p>{session.user.name}</p>
+//           </>
+//         ):(
+//           <button onClick={()=>signIn('google')}>Sign In</button>
+//           )
+//         }
   
-        <button onClick={()=>signOut()}>SignOut</button>
-    </div>
-  )
-}
+//         <button onClick={()=>signOut()}>SignOut</button>
+//     </div>
+//   )
+// }
 
-export default page
+// export default page
